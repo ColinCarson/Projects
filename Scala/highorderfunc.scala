fe160dc65caa335ef5d636ee32b088e442374064
Colin Carson <colincarson@gmail.com>
2016-10-08 00:42:07 +0100

    functions!

---
+++ Scala/highorderfunc.scala
@@ -0,0 +1,9 @@
+class Decorator(left: String, right: String) {
+    def layout[A](x: A) = left + x.toString() + right
+}
+
+object FuncTest extends App{
+    def apply(f: Int => String, v: Int) = f(v)
+    val decorator = new Decorator("[", "]")
+    println(apply(decorator.layout, 7))
+}
